2ebc9c24da7624990243a28867f339064c971d2f
ZenBarrier <anthony.j.barrera@gmail.com>
2015-12-11 17:14:53 -0500

    Set up routing pages skeleton.

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var codingBarrierApp = angular.module('codingBarrierApp', ['ngRoute']);
+
+// configure our routes
+codingBarrierApp.config(function ($routeProvider) {
+    $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl: 'pages/home.html',
+                controller: 'mainController'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl: 'pages/about.html',
+                controller: 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl: 'pages/contact.html',
+                controller: 'contactController'
+            })
+
+            .otherwise({
+                templateUrl: 'pages/404.html',
+                controller: 'notFoundController'
+            });
+});
+
+// create the controller and inject Angular's $scope
+codingBarrierApp.controller('mainController', function ($scope) {
+
+    // create a message to display in our view
+    $scope.message = 'Coding Barrier under construction.';
+});
+
+codingBarrierApp.controller('aboutController', function ($scope) {
+    $scope.message = 'This will be about me.';
+});
+
+codingBarrierApp.controller('contactController', function ($scope) {
+    $scope.message = 'This will be my contact page!';
+});
+
+codingBarrierApp.controller('notFoundController', function ($scope) {
+    $scope.message = 'Sorry, but the page you were trying to view does not exist.';
+});
