4ea8e39a28b0fbd06c2162d9c64a6d0c853ff8c8
ZenBarrier <anthony.j.barrera@gmail.com>
2015-12-18 18:10:54 -0500

    HTTP Get Error 500 will now retry upto 3 times until success. Redirects to blogger blog post if fail 4 times in a row.

--- js/blogPost.js
+++ js/blogPost.js
@@ -5,43 +5,60 @@
  */
 
 
-angular.module('codingBarrierApp').controller('blogPostController', function ($rootScope, $scope, $http, $location, $routeParams) {
-    $scope.message = 'This will be my blog page!';
-    var path = $routeParams.year + '/' + $routeParams.month + '/' + $routeParams.postPath;
+angular.module('codingBarrierApp').controller('blogPostController',
+        function ($rootScope, $scope, $http, $location, $routeParams, $window) {
+            $scope.message = 'This will be my blog page!';
+            var getCount = 0;
+            var path = $routeParams.year + '/' + $routeParams.month + '/' + $routeParams.postPath;
 
-    $http.get("https://www.googleapis.com/blogger/v3/blogs/4379869744446220679/posts/\n\
+
+            $scope.getPost = function () {
+
+                $http.get("https://www.googleapis.com/blogger/v3/blogs/4379869744446220679/posts/\n\
 bypath?path=/" + path + "&key=AIzaSyBe8zrqjTGEj92YfFvqEc4Yt993QW0q0cA")
-            .then(
-                    function (response) {
-                        blogRetrievedSuccess(response);
-                    },
-                    function (response) {
-                        blogRetrievedFail(response);
+                        .then(
+                                function (response) {
+                                    blogRetrievedSuccess(response);
+                                },
+                                function (response) {
+                                    blogRetrievedFail(response);
+                                });
+
+                var blogRetrievedSuccess = function (response) {
+                    $scope.post = response.data;
+                    $scope.disqus = 'pages/disqusModule.html';
+                    $rootScope.title = response.data.title + " | Blog";
+                    $rootScope.hasComments = true;
+                    console.log(response.data);
+
+                    DISQUS.reset({
+                        reload: true,
+                        config: function () {
+                            //this.page.identifier = response.data.id;
+                            this.page.url = response.data.url;
+                        }
                     });
+                };
 
-    var blogRetrievedSuccess = function (response) {
-        $scope.post = response.data;
-        $scope.disqus = 'pages/disqusModule.html';
-        $rootScope.title = response.data.title + " | Blog";
-        console.log(response.data);
+                var blogRetrievedFail = function (response) {
+                    $scope.post = "error";
+                    $rootScope.hasComments = false;
+                    console.log(response);
 
-        DISQUS.reset({
-            reload: true,
-            config: function () {
-                //this.page.identifier = response.data.id;
-                this.page.url = response.data.url;
-            }
+                    if (response.status === 500) {
+                        if (getCount < 3) {
+                            getCount++;
+                            $scope.getPost();
+                        } else{
+                            $window.location.href = 'http://blog.codingbarrier.com/'+path;
+                        }
+                    }
+
+                    if (response.status === 404) {
+                        $location.path('/404');
+                        $location.replace();
+                    }
+                };
+            };
+            $scope.getPost();
         });
-    };
-
-    var blogRetrievedFail = function (response) {
-        $scope.post = "error";
-        $rootScope.hasComments = false;
-        console.log(response);
-
-        if (response.status === 404) {
-            $location.path('/404');
-            $location.replace();
-        }
-    };
-});
