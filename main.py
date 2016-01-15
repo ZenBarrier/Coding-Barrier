5634536d6c2d286b340a4ca75eccf725d0feaf5c
ZenBarrier <anthony.j.barrera@gmail.com>
2016-01-15 16:41:49 -0500

    Index.html now serves as a static file.

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
+#import os
 import webapp2
-import os
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'index.html') 
-        f=open(path, "r")
-        self.response.write(f.read())
-        f.close()
+#class MainPage(webapp2.RequestHandler):
+#    def get(self):
+#        path = os.path.join(os.path.dirname(__file__), 'index.html') 
+#        f = open(path, "r")
+#        self.response.write(f.read())
+#        f.close()
 
 class ProjectRedirect(webapp2.RequestHandler):
     def get(self, proj):
-        self.redirect(projectLinks(proj))
+        self.redirect(project_links(proj))
 
 app = webapp2.WSGIApplication([
-    (r'/projects/(.*)', ProjectRedirect),
-    (r'/.*', MainPage),
-], debug=True)
+                              (r'/projects/(.*)', ProjectRedirect),
+                              #(r'/.*', MainPage),
+                              ], debug=True)
 
-def projectLinks(project):
+def project_links(project):
     return {
         "faceblr": "https://chrome.google.com/webstore/detail/faceblr/jblofdhkclkhbcajdmfhklcgolfofbck",
         "wearfull": "https://play.google.com/store/apps/details?id=com.zenbarrier.wearfull",
-    }.get(project,"/projects")
+    }.get(project, "/projects")
