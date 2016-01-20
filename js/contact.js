0dbd00ac93cbccdbff00442b5b5dd6c9456b7ea5
ZenBarrier <anthony.j.barrera@gmail.com>
2016-01-19 20:30:11 -0500

    Server sends depending on website domain.

--- js/contact.js
+++ js/contact.js
@@ -3,22 +3,31 @@ angular.module('codingBarrierApp').controller('contactController', function ($ro
     $rootScope.header = 'Contact';
     $rootScope.message = 'This will be my contact page!';
 
-    $scope.clear = function () {
-        $scope.form.name = '';
-        $scope.form.email = '';
-        $scope.form.subject = '';
-        $scope.form.message = '';
+    $scope.clear = function (form) {
+        if (form) {
+            form.$setPristine();
+            form.$setUntouched();
+        }
+        $scope.user = {};
     };
 
     $scope.sendEmail = function () {
+        if ($rootScope.siteName.indexOf("Coding") > 0) {
+            $scope.user.to="Anthony B <anthony@codingbarrier.com>";
+        } else {
+            $scope.user.to="Anthony B <mail@anthonybarrera.com>";
+        }
+        console.log($.param($scope.user));
         $http({
             method: 'POST',
             url: '/mail/',
-            data: $.param($scope.form), // pass in data as strings
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}  
+            data: $.param($scope.user), // pass in data as strings
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         })
                 .success(function () {
-                    
+
                 });
     };
+
+    $scope.clear();
 });
