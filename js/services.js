e227695e943692e43c1d9c4e561f178320390388
ZenBarrier <anthony.j.barrera@gmail.com>
2016-01-22 00:30:51 -0500

    Email validation service added.

--- js/services.js
+++ js/services.js
@@ -49,3 +49,19 @@ CodingBarrierService.factory('DisqusApi', ['$http', function () {
         return DisqusApi;
 
     }]);
+
+CodingBarrierService.factory('MailBoxApi', ['$http', function ($http) {
+
+        var urlBase = 'http://apilayer.net/api/check';
+        var apiKey = '?access_key=c6631fa3c4bdbede10afedd778b5dfd2';
+        var smpt = '&smtp=1';
+        var format = '&format=1';
+        var MailBoxApi = {};
+
+        MailBoxApi.getBlog = function (email) {
+            return $http.get(urlBase + apiKey + email + apiKey + smpt +format);
+        };
+
+        return MailBoxApi;
+
+    }]);
