b1dc6a0b3f5f251a2f8a7bd5de7ea79a9efb5c40
ZenBarrier <anthony.j.barrera@gmail.com>
2017-04-25 23:59:31 -0400

    Blog title changed to just Blog.

--- js/blog.js
+++ js/blog.js
@@ -8,7 +8,7 @@
 angular.module('codingBarrierApp')
         .controller('blogController', function ($rootScope, $sce, BloggerApi) {
 
-            $rootScope.header = 'Code Barrier Blog';
+            $rootScope.header = 'Blog';
             $rootScope.message = '';
             var blogPage = this;
 
